81d4eea49c8057a944c50fd9f8b2a873ee7e3b9e
siddharth-69 <duttasidb.1998@gmail.com>
2020-07-13 22:49:23 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React,{ Component} from 'react';
+
+import { Grid } from '@material-ui/core';
+import SearchBar from './components/SearchBar';
+import VideoDetail from './components/VideoDetail';
+import VideoList from './components/VideoList';
+import './App.css';
+
+import youtube from './api/youtube'
+
+
+class App extends Component{
+
+  state={
+    videos:[],
+    selectedVideo: null
+  }
+
+  componentDidMount(){
+    this.handleSubmit("macbook")
+  }
+
+  onVideoSelect = (video) =>{
+    this.setState({selectedVideo: video})
+  }
+
+  setSelectedVideo = (video) => {
+    this.setState({selectedVideo: video});
+  }
+
+  handleSubmit = async (searchTerm) => {
+    const response = await youtube.get("search", {
+      params: {
+        part: "snippet",
+        maxResults: 5,
+        key: "AIzaSyBFj8LyuNi9Zbbg9iSa8FERsU7fcLHMf5k",
+        q: searchTerm,
+      }
+    });
+
+    this.setState({videos: response.data.items,selectedVideo: response.data.items[0]});
+  }
+
+  render(){
+    const {selectedVideo,videos} = this.state
+    return (
+      <Grid style={{ justifyContent: "center" }} container spacing={12}>
+        <Grid item xs={10}>
+          <Grid container spacing={10}>
+            <Grid item xs={8}>
+              <SearchBar onSubmit={this.handleSubmit} />
+            </Grid>
+            <Grid item xs={8}>
+              <VideoDetail video={selectedVideo} />
+            </Grid>
+            <Grid item xs={2}>
+              <VideoList videos={videos} onVideoSelect={this.onVideoSelect} />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    )
+  }
+}
+
+export default App
